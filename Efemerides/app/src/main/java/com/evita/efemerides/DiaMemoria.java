3fa9cefadf7d98b6245d1cc07811a4e708633fe1
Alejandro Ghiringhelli <alejandroghiringhelli@gmail.com>
2022-03-21 20:42:14 -0300

    avance imagenes respuestas correctas incorrectas 21 marzo

--- Efemerides/app/src/main/java/com/evita/efemerides/DiaMemoria.java
+++ Efemerides/app/src/main/java/com/evita/efemerides/DiaMemoria.java
@@ -5,10 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DiaMemoria extends AppCompatActivity {
-    String [] preguntas = new String []{"Cuando pasó tal cosa?", "Cuando pasó tal otra?", "Y esta otra?"};
+    String [] preguntas = new String []{
+            "¿Qué se conmemora el 24 de marzo en Argentina?",
+            "¿Cómo se le llamó a este hecho?",
+            "¿Cuáles fueron las consecuencias más importante que dejo este proceso en el país?",
+            "Y esta otra?",
+            "Cuando pasó tal cosa?",
+            "Cuando pasó tal cosa?",
+            "Cuando pasó tal cosa?",
+    };
     String [][] respuestas = new String[10][5];
     int contadorPreguntas =0;
     private Button btn1;
@@ -16,6 +26,12 @@ public class DiaMemoria extends AppCompatActivity {
     private Button btn3;
     private Button btn4;
     private TextView txtpregunta;
+    private ImageView imgView1;
+    private ImageView imgView2;
+    private ImageView imgView3;
+    private ImageView imgView4;
+    private ImageView imgView5;
+    private ImageView imgView6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +40,14 @@ public class DiaMemoria extends AppCompatActivity {
 
 
 
-        respuestas[0][0] = "1950";
-        respuestas[0][1] = "1951";
-        respuestas[0][2] = "1952";
-        respuestas[0][3] = "1953";
-        respuestas[1][0] = "1954";
-        respuestas[1][1] = "1955";
-        respuestas[1][2] = "1956";
-        respuestas[1][3] = "1957";
+        respuestas[0][0] = "Día de la Independencia";
+        respuestas[0][1] = "Mundial Alemania 2006";
+        respuestas[0][2] = "Día de la Memoria la Verdad y la Justicia";
+        respuestas[0][3] = "Día del Veterano y de los caídos en la guerra de Malvinas";
+        respuestas[1][0] = "Proceso de Reorganización Federal";
+        respuestas[1][1] = "Proceso de Adaptación Nacional";
+        respuestas[1][2] = "Proceso de Adaptación Regional";
+        respuestas[1][3] = "Proceso de Reorganización Nacional";
         respuestas[2][0] = "1958";
         respuestas[2][1] = "1959";
         respuestas[2][2] = "1960";
@@ -52,6 +68,13 @@ public class DiaMemoria extends AppCompatActivity {
         btn2.setText(respuestas[0][1]);
         btn3.setText(respuestas[0][2]);
         btn4.setText(respuestas[0][3]);
+
+        imgView1 = (ImageView) findViewById(R.id.imgRpta1);
+        imgView2 = (ImageView) findViewById(R.id.imgRpta2);
+        imgView3 = (ImageView) findViewById(R.id.imgRpta3);
+        imgView4 = (ImageView) findViewById(R.id.imgRpta4);
+        imgView5 = (ImageView) findViewById(R.id.imgRpta5);
+        imgView6 = (ImageView) findViewById(R.id.imgRpta6);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,28 +113,29 @@ public class DiaMemoria extends AppCompatActivity {
 
     }
 
-
-
-
-
-
     public void cargarPreguntas(View view, int numero){
-
+        int idCorrecta = getResources().getIdentifier("rptacorrecta","drawable", getPackageName());
+        int idIncorrecta = getResources().getIdentifier("rtaincorrecta","drawable", getPackageName());
 
         switch (contadorPreguntas){
            case 0:{
-               if(numero == 1){
-                   btn1.setBackgroundColor(1);
-
+               if(numero == 1){ imgView1.setBackgroundColor(1);
+                   Toast.makeText(this, "Respuesta incorrecta", Toast.LENGTH_SHORT).show();
+                   //int id = getResources().getIdentifier("rptacorrecta","drawable", getPackageName());
+                   imgView1.setImageResource (idIncorrecta);
                }
-               if(numero == 2){
-                   btn1.setBackgroundColor(2);
+               if(numero == 2){ imgView1.setBackgroundColor(2);
+                   Toast.makeText(this, "Respuesta incorrecta", Toast.LENGTH_SHORT).show();
+                 //  int id = getResources().getIdentifier("rptacorrecta","drawable", getPackageName());
+                   imgView1.setImageResource (idIncorrecta);
                }
                if(numero == 3){
-                   btn1.setBackgroundColor(3);
-               }
-               if(numero == 4){
-                   btn1.setBackgroundColor(4);
+                   Toast.makeText(this, "Respuesta correcta", Toast.LENGTH_SHORT).show();
+                   imgView1.setImageResource (idCorrecta);   }
+               if(numero == 4){ imgView1.setBackgroundColor(2);
+                   Toast.makeText(this, "Respuesta incorrecta", Toast.LENGTH_SHORT).show();
+                   //int id = getResources().getIdentifier("rptacorrecta","drawable", getPackageName());
+                   imgView1.setImageResource (idIncorrecta);
                }
                btn1.setText(respuestas[1][0]);
                btn2.setText(respuestas[1][1]);
@@ -122,6 +146,10 @@ public class DiaMemoria extends AppCompatActivity {
                break;
                }
             case 1:{
+                if(numero == 1){ imgView2.setBackgroundColor(1);}
+                if(numero == 2){ imgView2.setBackgroundColor(2);}
+                if(numero == 3){ imgView2.setBackgroundColor(2);}
+                if(numero == 4){ imgView2.setBackgroundColor(2);}
                 btn1.setText(respuestas[2][0]);
                 btn2.setText(respuestas[2][1]);
                 btn3.setText(respuestas[2][2]);
@@ -131,6 +159,10 @@ public class DiaMemoria extends AppCompatActivity {
                 break;
             }
             case 2:{
+                if(numero == 1){ imgView3.setBackgroundColor(1);}
+                if(numero == 2){ imgView3.setBackgroundColor(2);}
+                if(numero == 3){ imgView3.setBackgroundColor(2);}
+                if(numero == 4){ imgView3.setBackgroundColor(2);}
                 btn1.setText(respuestas[3][0]);
                 btn2.setText(respuestas[3][1]);
                 btn3.setText(respuestas[3][2]);
@@ -140,11 +172,41 @@ public class DiaMemoria extends AppCompatActivity {
                 break;
             }
             case 3:{
+                if(numero == 1){ imgView4.setBackgroundColor(1);}
+                if(numero == 2){ imgView4.setBackgroundColor(2);}
+                if(numero == 3){ imgView4.setBackgroundColor(2);}
+                if(numero == 4){ imgView4.setBackgroundColor(2);}
                 btn1.setText(respuestas[4][0]);
                 btn2.setText(respuestas[4][1]);
                 btn3.setText(respuestas[4][2]);
                 btn4.setText(respuestas[4][3]);
                 txtpregunta.setText(preguntas[4]);
+                contadorPreguntas++;
+                break;
+            }
+            case 4:{
+                if(numero == 1){ imgView5.setBackgroundColor(1);}
+                if(numero == 2){ imgView5.setBackgroundColor(2);}
+                if(numero == 3){ imgView5.setBackgroundColor(2);}
+                if(numero == 4){ imgView5.setBackgroundColor(2);}
+                btn1.setText(respuestas[5][0]);
+                btn2.setText(respuestas[5][1]);
+                btn3.setText(respuestas[5][2]);
+                btn4.setText(respuestas[5][3]);
+                txtpregunta.setText(preguntas[5]);
+                contadorPreguntas++;
+                break;
+            }
+            case 5:{
+                if(numero == 1){ imgView6.setBackgroundColor(1);}
+                if(numero == 2){ imgView6.setBackgroundColor(2);}
+                if(numero == 3){ imgView6.setBackgroundColor(2);}
+                if(numero == 4){ imgView6.setBackgroundColor(2);}
+                btn1.setText(respuestas[6][0]);
+                btn2.setText(respuestas[6][1]);
+                btn3.setText(respuestas[6][2]);
+                btn4.setText(respuestas[6][3]);
+                txtpregunta.setText(preguntas[6]);
                 contadorPreguntas++;
                 break;
             }
