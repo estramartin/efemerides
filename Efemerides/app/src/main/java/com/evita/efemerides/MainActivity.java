d8447485a649d960c9f48d1084f15b78eaae6bdc
Alejandro Ghiringhelli <alejandroghiringhelli@gmail.com>
2022-03-24 11:24:22 -0300

    ya casi, faltan agregar preguntas

--- Efemerides/app/src/main/java/com/evita/efemerides/MainActivity.java
+++ Efemerides/app/src/main/java/com/evita/efemerides/MainActivity.java
@@ -6,17 +6,28 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button btnSalir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSalir = (Button) findViewById(R.id.button2);
+
+        btnSalir.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                finish();
+                System.exit(0);
+            }
+        });
 
      /*   DbHelper dbHelper = new DbHelper(MainActivity.this);
         SQLiteDatabase db  = dbHelper.getWritableDatabase();
